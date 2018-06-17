51c722b006a3b555523b14c9171b472b93d759c7
Andrew McGhie <mcghiea@gmail.com>
2018-06-17 12:51:51 -0500

    fix another typo in root index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/user");
 require("./Services/passport");
@@ -23,19 +23,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
-require('./routes/billingRoutes')(app);
+require("./routes/billingRoutes")(app);
 
-if (process.env.NOEDE_ENV === 'production') {
+if (process.env.NOEDE_ENV === "production") {
   //Express will serve up production assets
   //e.g., main.js/main.css, etc.
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
   //Express will serve up index.html
   //if it doesn't recoginze the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
