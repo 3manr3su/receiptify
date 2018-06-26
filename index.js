740d56a183d7db0bcfd00f80cb70a9e2bd1e13c5
Andrew McGhie <mcghiea@gmail.com>
2018-06-26 01:07:35 -0500

    update index.js to handle production routing of relative endpoints

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("./models/user");
 require("./models/receipt");
 require("./Services/passport");
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -25,6 +26,16 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/receiptRoutes")(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
