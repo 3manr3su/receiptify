84751bcd248fe339ccdb463464f93b32e9edf12c
Andrew McGhie <mcghiea@gmail.com>
2018-06-26 20:51:11 -0500

    update landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Landing = () => {
     return (
-        <div style={{ textAlign: "center" }}>
-            <h1>
+        <div className="container" style={{ textAlign: "center" }}>
+            <h3>
                 Welcome to Receiptify!
-            </h1>
+            </h3>
+            <h5 style={{fontFamily: "Courier New"}} >//please log in or click the Receiptify logo to upload some receipts//</h5>
         </div>
     );
 };
